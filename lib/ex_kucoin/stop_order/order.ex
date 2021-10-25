656f5e77ca35527e059d08e9e3341a3ece25773c
rodesousa <dessroberto@gmail.com>
2021-10-25 20:10:01 +0200

    feat: add stop order post
    Signed-off-by: rodesousa <dessroberto@gmail.com>

--- lib/ex_kucoin/stop_order/order.ex
+++ lib/ex_kucoin/stop_order/order.ex
@@ -8,11 +8,20 @@ defmodule ExKucoin.StopOrder.Order do
   @prefix "/api/v1/stop-order"
 
   @doc """
-  Request via this endpoint to get your current order list
+  Request via this endpoint to get your current stop order list
 
-  [API Docs](https://docs.kucoin.com/#list-orders)
+  [API Docs](https://docs.kucoin.com/#list-stop-orders)
   """
   def list(params \\ %{}, config \\ nil) do
-    get("#{@prefix}", params, config)
+    get(@prefix, params, config)
+  end
+
+  @doc """
+  Request via this endpoint to create a stop order list
+
+  [API](https://docs.kucoin.com/#place-a-new-order-2)
+  """
+  def create(params, config \\ nil) do
+    post(@prefix, params, config)
   end
 end
